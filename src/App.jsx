e47c7bd093285e145c57483a6aac023584212140
BenjiCoder02 <benkoshy02@gmail.com>
2024-06-15 15:55:23 -0400

    Create the navbar section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-
-import './App.css'
+import { BrowserRouter } from 'react-router-dom';
+import { Navbar } from './components';
+import './index.css';
 
 const App = () => {
   return (
-    <div>Portfolio 3.0</div>
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bf-center">
+          <Navbar />
+        </div>
+      </div>
+    </BrowserRouter>
   )
 }
 
-export default App
+export default App;
